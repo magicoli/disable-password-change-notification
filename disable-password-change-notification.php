a59bfde8233ecdb2959a9b4e5b1198668988f960
Olivier van Helden <olivier@van-helden.net>
2022-09-05 06:33:21 -0400

    Initial commit

---
+++ disable-password-change-notification.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Disable Password Change Notification
+ * Plugin URI:      https://github.com/magicoli/disable-password-change-notification
+ * Description:     Disable WordPress Password Change email notification
+ * Author:          Magiiic
+ * Author URI:      https://magiiic.com
+ * Text Domain:     disable-password-change-notification
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Disable_Password_Change_Notification
+ */
+
+// Your code starts here.
